f1de93e0ce7e1988bd5c66a5814b7a41ace34638
Damian33 <damian.cavaille@gmail.com>
2017-05-09 16:59:34 +0200

    Add files via upload

--- src/variables_operateurs.java
+++ src/variables_operateurs.java
@@ -13,9 +13,32 @@ public class variables_operateurs {
 		System.out.println("long : "+anneeLumiere);
 		
 		float pi = 3.141592653f; //4 octets
-		double nombre = 2.0f; // 8 octets
+		float nombre = 2.0f; // 8 octets
+		double division = 0.333333333333333333333333333333333333333333334d;
 		System.out.println("float : "+pi);
-		System.out.println("double : "+nombre);
+		System.out.println("float : "+nombre);
+		System.out.println("double : "+division);
+		
+		char caractere = 'A';
+		boolean question = true;
+		System.out.println("char : "+caractere);
+		System.out.println("boolean : "+question);
+		
+		//Première méthode de déclaration
+		String phrase;
+		phrase = "Titi et Grosminet";
+		//Deuxième méthode de déclaration
+		String str = new String();
+		str = "Une autre chaîne de caractères";
+		//Troisième méthode de déclaration
+		String string = "Une autre chaîne";
+		//Quatrième méthode de déclaration
+		String chaine = new String("Et une de plus !");
+		System.out.println(phrase);
+		System.out.println(str);
+		System.out.println(string);
+		System.out.println(chaine);
+
 	}
 
 }
