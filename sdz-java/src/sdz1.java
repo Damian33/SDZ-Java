d7f58f9d64e2620f49eb1455a71eaa16dbb930d6
Damian33 <damian.cavaille@gmail.com>
2017-05-10 22:17:39 +0200

    Tutoriel Java sur sdz

---
+++ sdz-java/src/sdz1.java
@@ -0,0 +1,12 @@
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World !\n");
+		System.out.println("C'est Damian");
+		System.out.print("Programme de folie en cours de dév");
+	}
+
+}
+
