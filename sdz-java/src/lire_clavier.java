a08d346c40bc58ac23749eb9df9c8cffe7e42937
Damian33 <damian.cavaille@gmail.com>
2017-05-16 17:25:43 +0200

    Add files via upload
    Classe lecture entrée clavier

---
+++ sdz-java/src/lire_clavier.java
@@ -0,0 +1,22 @@
+package lire_clavier;
+
+import java.util.Scanner;
+
+public class lire_clavier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Saisissez un entier : ");
+		int i = sc.nextInt();
+		System.out.println("Vous avez saisi : " + i);
+		System.out.println("Saisissez une chaîne : ");
+		//On vide la ligne avant d'en lire une autre
+		sc.nextLine();
+		String str = sc.nextLine();      
+		System.out.println("Vous avez saisi : " + str);
+		
+	}
+
+}
