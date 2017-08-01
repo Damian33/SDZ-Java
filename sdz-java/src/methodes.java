b2cbdc6a546ff309b363be62c006d51a3cf80c36
Damian33 <damian.cavaille@gmail.com>
2017-08-01 17:22:30 +0200

    Add files via upload

--- sdz-java/src/methodes.java
+++ sdz-java/src/methodes.java
@@ -26,5 +26,81 @@ public class methodes {
 		int posCaracPlus1 = posCarac + 1;
 		System.out.println("Le caractère en position " + posCaracPlus1 + " de la chaine '" + chaine + "' est : '"
 				+ chaine.charAt(posCarac) + "'");
+
+		// utilisation de la méthode substring
+		System.out.println("La chaine '" + chaine + "' du caractère 0 au caractère 3 vaut '" + chaine.substring(0, 3)
+				+ "'");
+
+		// utilisation des méthodes indexOf et lastIndexOf
+		System.out.println("Le caractère '!' se situe à la position " + chaine.indexOf("!")
+				+ " pour sa première occurence dans la chaîne '" + chaine + "'");
+		System.out.println("Le caractère '!' se situe à la position " + chaine.lastIndexOf("!")
+				+ " pour sa dernière occurence dans la chaîne '" + chaine + "'");
+
+		// utilisation des méthodes mathématiques
+		double X = 0.0;
+		X = Math.random();
+		System.out.println("X est un nombre aléatoire allant de 0 à 1 : " + X);
+
+		double sin = Math.sin(120);
+		System.out.println("Le sinus de 120° vaut : " + sin);
+		double cos = Math.cos(120);
+		System.out.println("Le cosinus de 120° vaut : " + cos);
+		double tan = Math.tan(120);
+		System.out.println("La tangent de 120° vaut : " + tan);
+		double abs = Math.abs(-45);
+		System.out.println("La valeur absolue de -45 est : " + abs);
+		double exp = Math.pow(10, 2);
+		System.out.println("La valeur de 10 puissance 2 est : " + exp);
+
+		// utilisation de la méthode arrondi
+		System.out.println("Le cosinus de 120° vaut : " + arrondi(cos,3) + " arrondi à 3 chiffre après la virgule");
+		
+		// utilisation de la méthode parcourirTableau
+		String tab[] = {"a","b","c","d","e","f"};
+		System.out.println(parcourirTableau (tab));
+		
+		// utilisation de la méthode parcourirTableau surchargée pour les int
+		int tab2[] = {1,2,3,4,5,6};
+		System.out.println(parcourirTableau (tab2));
+		
+		String[][] tab3 = {{"1", "2", "3", "4"}, {"toto", "titi", "tata"}};
+		System.out.println(parcourirTableau (tab3));
+		
 	}
+
+	// méthode arrondi
+	static double arrondi(double A, int B) {
+		return (double) ((int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
+	}
+	
+	// méthode parcourir tableau
+	static String parcourirTableau (String[] tabBis) {
+		String retour = "";
+		for (String str : tabBis) {
+			retour = retour + str + "\n";
+		}
+		return retour;
+	}
+	
+	// surcharge de la méthode parcourir tableau pour les int
+	static String parcourirTableau (int[] tabBis) {
+		String retour = "";
+		for (int str : tabBis) {
+			retour = retour + str + "\n";
+		}
+		return retour;
+	}
+	
+	// surcharge de la méthode parcourir tableau de String à deux dimensions
+	static String parcourirTableau (String[][] tabBis) {
+		String retour = "";
+		for (String sousStr[] : tabBis) {
+			for (String str : sousStr) {
+				retour = retour + str + "\n";
+			}
+		}
+		return retour;
+	}
+	
 }
