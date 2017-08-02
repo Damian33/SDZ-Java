8ef5d0e5a30570d4644a2670a8a7d2a3db609322
Damian33 <damian.cavaille@gmail.com>
2017-08-02 17:23:26 +0200

    Add files via upload

--- sdz-java/src/ClasseMain.java
+++ sdz-java/src/ClasseMain.java
@@ -3,8 +3,13 @@ package sdz;
 public class ClasseMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// construction d'un objet de type Ville
+		Ville ville = new Ville();
+		// construction d'un objet de type Ville avec paramètres
+		Ville ville2 = new Ville("Marseille",123456789,"France");
+		System.out.println(ville2.nomPays);
+		ville2.nomPays = "FRANCEEEEEEEE";
+		System.out.println(ville2.nomPays);
 	}
 
 }
--- sdz-java/src/Ville.java
+++ sdz-java/src/Ville.java
@@ -2,4 +2,23 @@ package sdz;
 
 public class Ville {
 
+	String nomVille;
+	String nomPays;
+	int nbreHabitants;
+
+	// Constructeur par défaut
+	public Ville() {
+		System.out.println("Création d'une ville !");
+		nomVille = "Inconnu";
+		nomPays = "Inconnu";
+		nbreHabitants = 0;
+	}
+	
+	// constructeur avec paramètres
+	public Ville(String pNom, int pNbre, String pPays) {
+		System.out.println("Créatin d'une ville avec paramètres");
+		nomVille = pNom;
+		nomPays = pPays;
+		nbreHabitants = pNbre;
+	}
 }
