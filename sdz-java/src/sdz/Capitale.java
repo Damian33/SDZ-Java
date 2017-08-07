fc5470b973208e1b97bd29cbd23a36a5b486ea4b
Damian33 <damian.cavaille@gmail.com>
2017-08-07 22:40:43 +0200

    modification des classes Capitale.java et Heritage.java

--- sdz-java/src/sdz/Capitale.java
+++ sdz-java/src/sdz/Capitale.java
@@ -2,10 +2,34 @@ package sdz;
 
 public class Capitale extends Ville {
 	private String monument;
-	
-	public Capitale () {
+
+	public Capitale() {
 		super(); // appel le constructeur de la classe mère
 		this.nomVille = "InconnuInconnu";
 		monument = "aucun";
+	}
+
+	public Capitale(String nom, int hab, String pays, String monument) {
+		super(nom, hab, pays);
+		this.monument = monument;
+	}
+
+	public String decrisToi() {
+		String str = super.decrisToi() + "\n\t ==>>" + this.monument + " en est un monument";
+		System.out.println("Invocation de super.decrisToi()");
+
+		return str;
+	}
+
+	/**
+	 * @return le nom du monument
+	 */
+	public String getMonument() {
+		return monument;
+	}
+
+	// Définit le nom du monument
+	public void setMonument(String monument) {
+		this.monument = monument;
 	}
 }
--- sdz-java/src/sdz/Heritage.java
+++ sdz-java/src/sdz/Heritage.java
@@ -5,5 +5,34 @@ public class Heritage {
 	public static void main(String[] args) {
 		Capitale cap = new Capitale();
 		System.out.println(cap.decrisToi());
+
+		Capitale capParams = new Capitale("Paris", 1000, "France", "Tour Eiffel");
+		System.out.println(capParams.decrisToi());
+
+		// Définition d'un tableau de villes null
+		Ville[] tableau = new Ville[6];
+
+		// Définition d'un tableau de noms de villes et un autre de nombres
+		// d'habitants
+		String[] tab = { "Marseille", "lille", "caen", "lyon", "paris", "nantes" };
+		int[] tab2 = { 123456, 78456, 654987, 75832165, 1594, 213 };
+
+		// Les trois premiers éléments du tableau seront des villes,
+		// et le reste, des capitales
+		for (int i = 0; i < 6; i++) {
+			if (i < 3) {
+				Ville V = new Ville(tab[i], tab2[i], "france");
+				tableau[i] = V;
+			}
+			else {
+				Capitale C = new Capitale(tab[i], tab2[i], "france", "la tour Eiffel");
+				tableau[i] = C;
+			}
+		}
+
+		// La méthode decrisToi() est polymorphe, elle peut être appelé depuis une objet issu des classes Capitale et Ville
+		for (Ville V : tableau) {
+			System.out.println(V.decrisToi() + "\n");
+		}
 	}
 }
